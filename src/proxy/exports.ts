daa0a5bc4560ae1b4771253ae86199da59cf4a14
Rachelle <rachellerathbone@gmail.com>
2026-03-26 14:53:06 +1100

    sr 02

---
+++ src/proxy/exports.ts
@@ -0,0 +1,33 @@
+/**
+ * Subpath entry `multicorn-shield/proxy` for hosted proxy and tooling.
+ *
+ * @module proxy/exports
+ */
+
+export {
+  parseJsonRpcLine,
+  extractToolCallParams,
+  buildBlockedResponse,
+  buildSpendingBlockedResponse,
+  buildInternalErrorResponse,
+  buildServiceUnreachableResponse,
+  buildAuthErrorResponse,
+  extractServiceFromToolName,
+  extractActionFromToolName,
+  type JsonRpcRequest,
+  type JsonRpcResponse,
+  type JsonRpcError,
+  type ToolCallParams,
+} from "./interceptor.js";
+
+export {
+  fetchGrantedScopes,
+  findAgentByName,
+  deriveDashboardUrl,
+  ShieldAuthError,
+  type AgentRecord,
+} from "./consent.js";
+
+export { createLogger, isValidLogLevel, type ProxyLogger, type LogLevel } from "./logger.js";
+
+export { validateScopeAccess } from "../scopes/scope-validator.js";
